bbf57b52e9aaf2c254c95daae7aa08af332873f2
Perki <perki@pryv.com>
2025-04-28 18:27:41 +0200

    First form for patient

--- patient-controler.js
+++ patient-controler.js
@@ -8,15 +8,64 @@
 
 window.onload = (event) => {
   stateChange('loggedOut');
-  drLib.showLoginButton('login-button', stateChange);
+  patientLib.showLoginButton('login-button', stateChange);
+  document.getElementById('submit-button').addEventListener("click", submitForm);
 };
 
 function stateChange(state) {
   if (state === 'loggedIN') {
     document.getElementById('please-login').style.visibility = 'hidden';
-    document.getElementById('data-view').style.visibility = 'visible';
+    document.getElementById('card-form').style.visibility = 'visible';
+    updateFormContent();
   } else {
     document.getElementById('please-login').style.visibility = 'visible';
-    document.getElementById('data-view').style.visibility = 'hidden';
+    document.getElementById('card-form').style.visibility = 'hidden';
   }
 }
+
+
+/**
+ * Take the from content from the definition and actual values and create the HTML
+ */
+async function updateFormContent() {
+  const formContent = await patientLib.getFormContent();
+  console.log('Form content:', formContent);
+ 
+  document.getElementById('inputs-list').innerHTML = ''; // Clear previous content
+  for (let i = 0; i < formContent.length; i++) {
+    const formField = formContent[i];
+    const fieldId = formField.id;
+    const fieldValue = formField.value || '';
+    const fieldType = formField.type;
+    const fieldLabel = formField.label;
+    
+    // Create the HTML for the form field
+    let fieldHTML = `\n<BR><label for="${fieldId}">${fieldLabel}</label>`;
+    if (fieldType === 'text' || fieldType === 'number') {
+      fieldHTML += `<input type="${fieldType}" id="${fieldId}" value="${fieldValue}" class="form-control"/>`;
+    } else if (fieldType === 'select') {
+      fieldHTML += `<select id="${fieldId}">`;
+      for (const option of fieldValue) {
+        fieldHTML += `<option value="${option.value}">${option.label}</option>`;
+      }
+      fieldHTML += `</select>`;
+    }
+    // Append the HTML to the form
+    document.getElementById('inputs-list').innerHTML += fieldHTML;
+  }
+}
+
+/**
+ * Submit the form and send the data to the API
+ */
+async function submitForm() {
+  const values = {};
+  const formContent = await patientLib.getFormContent();
+  for (let i = 0; i < formContent.length; i++) {
+    const field = formContent[i];
+    const fieldId = field.id;
+    const fieldValue = document.getElementById(fieldId).value.trim();
+    values[field.id] = fieldValue; // Store the value in the values object
+  }
+  await patientLib.handleFormSubmit(values);
+};
--- patient-lib.js
+++ patient-lib.js
@@ -1,8 +1,9 @@
 let connection = null;
 
-const drLib = {
-  showLoginButton
-
+const patientLib = {
+  showLoginButton,
+  handleFormSubmit,
+  getFormContent,
 }
 
 function showLoginButton (loginSpanId, stateChangeCallBack) {
@@ -14,7 +15,28 @@ function showLoginButton (loginSpanId, stateChangeCallBack) {
       requestingAppId: 'demo-dr-form-patient', // to customize for your own app
       requestedPermissions: [ 
         {
-          streamId: '*',
+          streamId: 'profile',
+          defaultName: 'Profile',
+          level: 'manage' 
+        },
+        {
+          streamId: 'family',
+          defaultName: 'Family',
+          level: 'manage' 
+        },
+        {
+          streamId: 'fertility',
+          defaultName: 'Fertility',
+          level: 'manage' 
+        },
+        {
+          streamId: 'body-height',
+          defaultName: 'Body height',
+          level: 'manage' 
+        },
+        {
+          streamId: 'body-weight',
+          defaultName: 'Body weight',
           level: 'manage' 
         }
       ],
@@ -22,7 +44,12 @@ function showLoginButton (loginSpanId, stateChangeCallBack) {
         'app-web-auth:description': {
           'type': 'note/txt',
           'content': 'This app allows to fill a form and share information with your doctor.'
-        } 
+        },
+        'app-web-auth:ensureBaseStreams': [
+          {id: 'body', name: 'Body metrics'},
+          {id: 'body-height', name: 'Body height', parentId: 'body'},
+          {id: 'body-weight', name: 'Body weight', parentId: 'body'}
+        ]
       },
     }
   };
@@ -45,7 +72,146 @@ function showLoginButton (loginSpanId, stateChangeCallBack) {
   }
 }
 
+// Creates the streams structure for the patient account after the user has logged in
 async function initPatientAccount (connection) {
- 
+  const patientBaseStreams = [
+    // profile
+    {id: 'profile-name', name: 'Name', parentId: 'profile'},
+    {id: 'profile-nationality', name: 'Nationality', parentId: 'profile'},
+    {id: 'profile-date-of-birth', name: 'Date of Birth', parentId: 'profile'},
+    {id: 'profile-location', name: 'Location', parentId: 'profile'},
+    {id: 'profile-sex', name: 'Sex', parentId: 'profile'},
+    // family
+    {id: 'family-children', name: 'Children', parentId: 'family'},
+    // fertility
+    {id: 'fertility-miscarriages', name: 'Miscarriages', parentId: 'fertility'},
+    {id: 'fertility-traings', name: 'Trainings', parentId: 'fertility'},
+    {id: 'fertility-cycles', name: 'Cycles', parentId: 'fertility'},
+    {id: 'fertility-cycles-charted-extimation', name: 'Cycles charted estimation', parentId: 'fertility'},
+    {id: 'fertility-ttc-tta', name: 'Trying to conceive / Avoiding pregnancy', parentId: 'fertility'},
+  ];
+  const apiCalls = patientBaseStreams.map(stream => ({
+    method: 'streams.create',
+    params: {
+      id: stream.id,
+      name: stream.name,
+      parentId: stream.parentId
+    }
+  }));
+  // create stream structure (even if already exists)
+  const res = await connection.api(apiCalls);
+  console.log('## Patient account streams created', res);
   console.log('## Patient account initialized')
+}
+
+// ---------------- form content ---------------- //
+
+const formContent = [
+  {
+    streamId: 'profile-name',
+    eventType: 'contact/name',
+    contentField: 'name',
+    type: 'text',
+    label: 'Name',
+  },
+  {
+    streamId: 'profile-name',
+    eventType: 'contact/surname',
+    contentField: 'surname',
+    type: 'text',
+    label: 'Surname',
+  },
+  {
+    streamId: 'profile-nationality',
+    eventType: 'contact/nationality',
+    type: 'text',
+    label: 'Nationality',
+  },
+];
+
+let formInitialized = null;
+async function getFormContent () {
+  if (formInitialized) { return formContent; }
+  formInitialized = true; // prevent multiple calls to this function
+
+  // get the values from the API
+  const apiCalls = formContent.map(field => ({
+    method: 'events.get',
+    params: {
+      streams: [field.streamId],
+      types: [field.eventType],
+      limit: 1,
+    }
+  }));
+
+  const res = await connection.api(apiCalls);
+  for (let i = 0; i < res.length; i++) {
+    const e = res[i];
+    const field = formContent[i];
+    field.id = 'field-' + i; // generate a unique id for the field
+    console.log('## getFormContent ' + i, e);
+    if (e.events && e.events.length > 0) {
+      const event = e.events[0];
+      field.value = event.content;
+      field.eventId = event.id; // will allow t track if the event is to be updated
+    } 
+  }
+  return formContent;
+};
+
+// ---------------- create / update data ---------------- //
+async function handleFormSubmit (values) {
+  const apiCalls = [];
+ for (const field of formContent) {
+    const streamId = field.streamId;
+    const eventType = field.eventType;
+    const eventId = field.eventId;
+    const value = values[field.id];
+
+    if (value === '' && eventId) {
+      // delete the event
+      apiCalls.push({
+        method: 'events.delete',
+        params: {
+          id: eventId,
+        }
+      });
+      continue;
+    }
+
+    if (value === field.value || value === '') {
+      // no change
+      continue;
+    }
+
+    if (eventId) {
+      // update the event
+      apiCalls.push({
+        method: 'events.update',
+        params: {
+          id: eventId,
+          update: {
+            content: value
+          }
+        }
+      });
+      continue;
+    } 
+    // create a new event
+    apiCalls.push({
+      method: 'events.create',
+      params: {
+        streamId: streamId,
+        type: eventType,
+        content: value,
+      }
+    });
+  }
+  if (apiCalls.length === 0) {
+    console.log('## No changes to submit');
+    return;
+  }
+  // send the API calls
+  const res = await connection.api(apiCalls);
+  console.log('## Form submitted', res);
 }
